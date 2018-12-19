6a4011bcf597f5e120dd77a296b73ba0400f0ffe
Tyler King <stking74@gmail.com>
2018-12-19 11:36:47 -0500

    aggregate weekly data
    add function to aggregate all data dumps from previous week into a master list of orders

--- Sleeper.py
+++ Sleeper.py
@@ -2,6 +2,7 @@
 
 import esipy as api
 import datetime
+import time
 import h5py
 import numpy as np
 import pickle
@@ -23,6 +24,7 @@ def crawldir(topdir=[], ext='sxm'):
     Crawls through given directory topdir, returns list of all files with 
     extension matching ext
     '''
+    import re
     fn = dict()
     for root, dirs, files in os.walk(topdir):
               for name in files:
@@ -37,12 +39,13 @@ def crawldir(topdir=[], ext='sxm'):
                         fn[root] = [addname]    
     return fn
 
-class Sleeper(object):
+class Sleeper():
     
     def __init__(self, app, client):
         self.app = app
         self.client = client
-        self.root_dir = ''
+        self.root_dir = os.getcwd()
+        self.store_dir = os.path.join(self.root_dir, 'data_dumps')
         self.settings_fname = 'sleeper_settings.sl'
         return
     
@@ -71,19 +74,23 @@ class Sleeper(object):
         print('Scraping market data')
         print('Region:')
         for region in self.region_list.items():
+            pull_time = datetime.datetime.now()
             name, region_data = region
             print(name)
             region_id = region_data['region_id']
             unpacked = []
             orders[name] = self._request_region_market_orders(region_id, order_type='all')
+            for order in orders[name]:
+                order['timestamps'] = pull_time
+            time.sleep(0.5)
         refresh_time = datetime.datetime.now()
         filename_timestamp = refresh_time.strftime('%c')
-        os.chdir('data_dumps')
+        os.chdir(self.store_dir)
         pik_filename = 'market_dump-'+str(refresh_time)[:10]+'.pik'
         pik_file = open(pik_filename, 'wb')
         pickle.dump(orders, pik_file)
         pik_file.close()
-        os.chdir('..')
+        os.chdir(self.root_dir)
         
         return
     
@@ -163,8 +170,73 @@ class Sleeper(object):
     def _new_settings_file_():
         return
     
+    def _aggregate_weekly_(self):
+        week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
+        weekly_dumps = []
+        os.chdir(self.store_dir)
+        contents = os.listdir(self.store_dir)
+        files = []
+        for item in contents:
+            if os.path.isfile(item):
+                fname, ext = item.split('.')
+                if ext == 'pik':
+                    weekly_dumps.append(item)
+        catalog = []
+        order_ids = set()
+        order_id_list = []
+        for file in weekly_dumps:
+            f_timestamp = file[12:22]
+            timestamp = datetime.datetime.strptime(f_timestamp, '%Y-%m-%d')
+            if timestamp > week_ago:
+                print('processing file:', file)
+                with open(file, 'rb') as f:
+                    data_dump = pickle.load(f)
+                for key, region in data_dump.items():
+                    print('%s: %i'%(key, len(region)))
+                    oids = [order['order_id'] for order in region]
+                    new_oids = []
+                    old_oids = []
+                    for idx, oid in enumerate(oids):
+                        if oid in order_ids:
+                            old_oids.append(idx)
+                        else:
+                            new_oids.append(idx)
+                    for order in region:
+                        ti = time.time()
+                        oid = order['order_id']
+                        if oid not in order_ids:
+                            order_ids.add(oid)
+                            order_id_list.append(oid)
+                            order['volume_remain'] = [order['volume_remain']]
+                            try:
+                                order['timestamps'] = [order['timestamps']]
+                            except KeyError:
+                                order['timestamps'] = [timestamp]
+                            order['price'] = [order['price']]
+                            catalog.append(order)
+                        else:
+                            i = order_id_list.index(oid)
+                            check = catalog[i]
+                            cid = check['order_id']
+                            if cid == oid:
+                                check['volume_remain'].append(order['volume_remain'])
+                                check['price'].append(order['price'])
+                                try:
+                                    check['timestamps'].append(order['timestamps'])
+                                except KeyError:
+                                    check['timestamps'].append(timestamp)
+                                catalog[i] = check
+                                te = time.time() - ti
+#                                print(oid, len(order_ids), te)
+                                continue
+                        te = time.time() - ti
+#                        print(oid, len(order_ids), te)
+                        
+        return catalog
+        
+    
 MarketDump = Sleeper(app, client)
 #MarketDump._load_settings_file_()
-MarketDump._update_region_list()
-MarketDump.market_dump()
-orders = MarketDump.aggregate_data('data_dumps')
+#MarketDump._update_region_list()
+#MarketDump.market_dump()
+catalog = MarketDump._aggregate_weekly_()
